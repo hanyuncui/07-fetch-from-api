fe8705921660032a6936bfd62d29a96006ec39e6
Hanyun(Cecilia) Cui <hanyunc@umich.edu>
2025-10-28 17:40:31 +0000

    fetch from api

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ const punchline = document.getElementById('punchline');
 // Add an event listener to the button
 jokeBtn.addEventListener('click', function() {
   console.log('Getting a joke...');
+
+  // Fetch a random joke from the Official Joke API
+  fetch(`https://official-joke-api.appspot.com/random_joke`)
+    .then(response => response.json()) // Parse the JSON from the response
+    .then(data => {
+      // Display the joke on the page
+      setup.textContent = data.setup;
+      punchline.textContent = data.punchline;
+    });
+      
 });
